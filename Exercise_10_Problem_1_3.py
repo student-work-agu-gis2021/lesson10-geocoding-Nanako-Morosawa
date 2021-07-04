464e57a4736a3e88f93088c2d59bae890e3dd7dc
NanakoMorosawa <81939130+Nanako-Morosawa@users.noreply.github.com>
2021-07-04 17:45:49 +0000

    I used sjoin() for spatial join with the paramter how="inner", op="intersects".

--- Exercise_10_Problem_1_3.py
+++ Exercise_10_Problem_1_3.py
@@ -114,7 +114,7 @@ print(geodata.head())
 # YOUR CODE HERE 9
 # Read population grid data for 2018 into a variable `pop`. 
 import requests  
-import geojson 
+import geojson  
 url =  'https://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-mesh500h30.html#prefecture13' 
 params = dict(service='WFS',version='2.0.0',request='GetFeature',
               typeName='500m_mesh_2018_13.shp',outputFormat='json')
